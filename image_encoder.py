1e21e59d867d12c7e6e60102166289bb3922e62d
yashtapre77 <yashtapre77@gmail.com>
2025-03-31 12:00:00 +0530

    storing encoding in pickle

--- image_encoder.py
+++ image_encoder.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 import face_recognition
-
+import pickle
 
 def encode_faces(directory_path): 
     # path of input images to detect in frames
@@ -11,13 +11,13 @@ def encode_faces(directory_path):
 
     # creating all images path and storing these images in a list 
     images_name_list = os.listdir(folder_path)
-    image_path_dict = {}
+    images_path_dict = {}
     face_encodings_dict = {}
 
     for path in images_name_list:
         face_id  =  path.split('.')[0]
         full_path = os.path.join(folder_path, path)
-        image_path_dict[face_id] = full_path
+        images_path_dict[face_id] = full_path
         img = cv2.imread(full_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         face_encodings_dict[face_id] = face_recognition.face_encodings(img)[0]
@@ -25,6 +25,9 @@ def encode_faces(directory_path):
     return face_encodings_dict 
 
 
-# ret = encode_faces('Images')
+ret = encode_faces('Images')
+file = open('face_encodings.pickle', 'wb')
+pickle.dump(ret, file)
+file.close()
 # print("Number of different faces stored as input for future detection", len(ret))
 # print("Face encodings dictionary:", ret)
--- main.py
+++ main.py
@@ -2,30 +2,39 @@ import cv2
 import numpy as np
 import streamlit as st
 import os
+import pickle 
+import face_recognition
 
 # path of input images to detect in frames
 folder_path = 'Images'
 if not os.path.exists(folder_path):   # if path does not exist, create it
     os.makedirs(folder_path)
 
-# creating all images path and storing these images in a list 
-model_path_list = os.listdir(folder_path)
-model_path_list = [os.path.join(folder_path, path) for path in model_path_list]
+# creating all images path and storing these images in a list, along with their ids
+images_name_list = os.listdir(folder_path)
+images_path_dict = {}
 
-image_mode_list = []
-for path in model_path_list: 
-    image_mode_list.append(path)
+for path in images_name_list: 
+    face_id  =  path.split('.')[0]
+    full_path = os.path.join(folder_path, path)
+    images_path_dict[face_id] = full_path
 
-print("Number of different faces stored as input for future detection",len(image_mode_list))
+
+# load encoded image data from pickle file
+file = open("face_encodings.pickle",'rb')
+face_encodings_dict = pickle.load(file)
+file.close()
+
+print("Face encodings dictionary:", face_encodings_dict)
+print("Number of different faces stored as input for future detection",len(images_path_dict))
 
 
 # Capturing video from webcam
 cap  = cv2.VideoCapture(0)
 
+# setting streamlit page 
 st.title("Face Attendance System")
-
 frame_placeholder = st.empty()
-
 stop_button = st.button("Stop Detection")
 
 while cap.isOpened() and not stop_button:
@@ -47,3 +56,17 @@ while cap.isOpened() and not stop_button:
 # Release the video capture object
 cap.release()   
 cv2.destroyAllWindows()
+
+
+
+
+def check_face_encodings(frame):
+    # Convert the frame to RGB and resize it for faster processing to 1/4 size
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.resize(rgb_frame, (0, 0), None, 0.25, 0.25)
+
+    # Find all face locations and encodings in the frame
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    return face_locations, face_encodings
