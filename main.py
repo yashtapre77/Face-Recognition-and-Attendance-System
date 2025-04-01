5aea3b149a4a6ac923d40ac0fc97c08cd68ce044
yashtapre77 <yashtapre77@gmail.com>
2025-04-01 07:30:00 +0530

    added face comparison

--- main.py
+++ main.py
@@ -5,13 +5,29 @@ import os
 import pickle 
 import face_recognition
 
+# Load the face encodings from the pickle file
 file = open("face_encodings.pickle",'rb')
 face_encodings_dict = pickle.load(file)
 file.close()
 
-# print("Face encodings dictionary:", face_encodings_dict)
+def compare_faces(frame, face_encodings ):
+    i = 0
+    matches = None
+    face_dis = None
+    # Compare the face encodings with the known faces
+    for encode_face in face_encodings:
+        matches = face_recognition.compare_faces(list(face_encodings_dict.values()), encode_face)
+        face_dis = face_recognition.face_distance(list(face_encodings_dict.values()), encode_face)
 
-def check_face_encodings(frame):
+        print(i," Matches:", matches)
+        print(i," Face distances:", face_dis)
+        i += 1
+
+
+    return matches, face_dis
+
+
+def encode_video_faces(frame):
     # Convert the frame to RGB and resize it for faster processing to 1/4 size
     rgb_frame = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
 
@@ -19,47 +35,31 @@ def check_face_encodings(frame):
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    for encode_face, face_loc in zip(face_encodings, face_locations):
-        # Compare the face encodings with the known faces
-        matches = face_recognition.compare_faces(list(face_encodings_dict.values()), encode_face)
-        face_dis = face_recognition.face_distance(list(face_encodings_dict.values()), encode_face)
-        name = "Unknown"
+    # for encode_face, face_loc in zip(face_encodings, face_locations):
+    #     # Compare the face encodings with the known faces
+    #     matches = face_recognition.compare_faces(list(face_encodings_dict.values()), encode_face)
+    #     face_dis = face_recognition.face_distance(list(face_encodings_dict.values()), encode_face)
+    #     name = "Unknown"
 
-        # If a match is found, get the name of the matched face
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = list(face_encodings_dict.keys())[first_match_index]
+    #     # If a match is found, get the name of the matched face
+    #     if True in matches:
+    #         first_match_index = matches.index(True)
+    #         name = list(face_encodings_dict.keys())[first_match_index]
 
-        # Draw a rectangle around the face and put the name on it
-        top, right, bottom, left = face_loc
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-        cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    #     # Draw a rectangle around the face and put the name on it
+    #     top, right, bottom, left = face_loc
+    #     top *= 4
+    #     right *= 4
+    #     bottom *= 4
+    #     left *= 4
+    # print(face_di)
+    # for i in range(len(face_locations)):
+    #     top, right, bottom, left = face_locations[i]
+    #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+    # #     cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
     return frame, face_locations, face_encodings
 
-# # path of input images to detect in frames
-# folder_path = 'Images'
-# if not os.path.exists(folder_path):   # if path does not exist, create it
-#     os.makedirs(folder_path)
-
-# # creating all images path and storing these images in a list, along with their ids
-# images_name_list = os.listdir(folder_path)
-# images_path_dict = {}
-
-# for path in images_name_list: 
-#     face_id  =  path.split('.')[0]
-#     full_path = os.path.join(folder_path, path)
-#     images_path_dict[face_id] = full_path
-
-
-# load encoded image data from pickle file
-
-# print("Number of different faces stored as input for future detection",len(images_path_dict))
 
 
 # Capturing video from webcam
@@ -78,7 +78,8 @@ while cap.isOpened() and not stop_button:
 
     # Convert the frame to RGB, cv2 uses BGR by default and Streamlit uses RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    new_frame, face_locations, face_encodings = check_face_encodings(rgb_frame)
+    new_frame, face_locations, face_encodings = encode_video_faces(rgb_frame)
+    matches, face_dis = compare_faces(rgb_frame, face_encodings)
 
 
     # Display the frame in Streamlit
